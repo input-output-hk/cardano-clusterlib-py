ec86ab8d80821caba623b45f7fbfabb6c0b715df
saratomaz <sara.tomaz@iohk.io>
2022-02-09 09:47:48 +0000

    add funcionality to generate a non extended verification key

--- cardano_clusterlib/clusterlib.py
+++ cardano_clusterlib/clusterlib.py
@@ -1291,6 +1291,37 @@ class ClusterLib:
         self._check_outfiles(out_file)
         return out_file
 
+    def gen_non_extended_verification_key(
+        self, key_name: str, verification_key_file: FileType, destination_dir: FileType = "."
+    ) -> Path:
+        """Generate a non extenteded key from a verification key.
+
+        Args:
+            key_name: A name of the key.
+            verification_key_file: A path to verification key file.
+            destination_dir: A path to directory for storing artifacts (optional).
+
+        Returns:
+            Path: A path to the generated non extented verification key file.
+        """
+        destination_dir = Path(destination_dir).expanduser()
+        out_file = destination_dir / f"{key_name}.vkey"
+        self._check_files_exist(out_file)
+
+        self.cli(
+            [
+                "key",
+                "non-extended-key",
+                "--extended-verification-key-file ",
+                str(verification_key_file),
+                "--verification-key-file",
+                str(out_file),
+            ]
+        )
+
+        self._check_outfiles(out_file)
+        return out_file
+
     def get_ledger_state(self) -> dict:
         """Return the current ledger state info."""
         ledger_state: dict = json.loads(self.query_cli(["ledger-state"]))
