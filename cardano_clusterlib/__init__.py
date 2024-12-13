8004b28d5369b5ba47428d4c7458fc112210343d
Martin Kourim <martin.kourim@iohk.io>
2024-12-13 19:06:42 +0100

    chore: update pre-commit hooks and fix imports
    - Bump pre-commit hooks versions in .pre-commit-config.yaml
    - Reformat code with the new ruff
    - Update ignore rules in pyproject.toml for ruff

--- cardano_clusterlib/__init__.py
+++ cardano_clusterlib/__init__.py
@@ -4,6 +4,6 @@ from cardano_clusterlib.clusterlib_klass import ClusterLib
 from cardano_clusterlib.exceptions import CLIError
 
 __all__ = [
-    "ClusterLib",
     "CLIError",
+    "ClusterLib",
 ]
